d8e3e42a3d3c1a44fbcac17a4040d038395c95c3
Mohit Jangra <Mohit Jangra>
2021-12-21 11:25:56 +0530

    Added code for Day 1: Band Name Generator

---
+++ Day_1_Band_Name_Generator/main.py
@@ -0,0 +1,5 @@
+print("Hello There\n")
+print("Welcome to the Band Name Generator.")
+street = input("What is the name of the your city?\n")
+pet = input("What is your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
